086ece62587263ce5070d81ee6b93f2f5500b01b
rommoise <rommoise@lab126.com>
2017-02-07 14:25:00 -0800

    Adding the humanize files

---
+++ index.js
@@ -0,0 +1,23 @@
+angular.module('humanize', [])
+  .filter('humanizeTime', () =>
+    () => humanize.time())
+  .filter('humanizeDate', (...args) =>
+    humanize.date.apply(humanize, args))
+  .filter('humanizeNumberFormat', (...args) =>
+    humanize.numberFormat.apply(humanize, args))
+  .filter('humanizeNaturalDay', (...args) =>
+    humanize.naturalDay.apply(humanize, args))
+  .filter('humanizeRelativeTime', (...args) =>
+    humanize.relativeTime.apply(humanize, args))
+  .filter('humanizeOrdinal', (...args) =>
+    humanize.ordinal.apply(humanize, args))
+  .filter('humanizeFilesize', (...args) =>
+    humanize.filesize.apply(humanize, args))
+  .filter('humanizeLinebreaks', (...args) =>
+    humanize.linebreaks.apply(humanize, args))
+  .filter('humanizeNl2br', (...args) =>
+    humanize.nl2br.apply(humanize, args))
+  .filter('humanizeTruncateChars', (...args) =>
+    humanize.truncatechars.apply(humanize, args))
+  .filter('humanizeTruncateWords', (...args) =>
+    humanize.truncatewords.apply(humanize, args));
